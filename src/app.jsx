5807f3b92ac392963428caa82ef38517700bdad0
HazelZ <525618432@qq.com>
2018-04-07 17:44:06 +0800

    init

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import './index.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
+ReactDOM.render(
+	<div>
+		<h1>HI,hazel</h1>
+		<i className='fa fa-eye'></i>
+	</div>
+	,document.getElementById('app')
+)
