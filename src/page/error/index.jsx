f656d186a2c35238bf9813e411fc0bbe8804e7be
HazelZ <525618432@qq.com>
2018-04-11 22:39:45 +0800

    error page

---
+++ src/page/error/index.jsx
@@ -0,0 +1,24 @@
+import React,{ Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import PageTitle from 'component/_pageTitle/index';
+
+class Error extends Component {
+	render(){
+		return(
+			<div id="page-wrapper">
+				<div id="page-inner">
+					<PageTitle title='出错啦！~'/>
+					<div className="row">
+						<div className="col-md-12">
+						<span>找不到该路径，</span>
+						<Link to='/'>点我返回首页</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+			)
+	}
+}
+
+export default Error;
