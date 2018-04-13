4fd1366bdde3e1f270fd3435844ddec8a5d74e15
HazelZ <525618432@qq.com>
2018-04-13 16:28:34 +0800

    product-list page

--- src/page/product/index/index.jsx
+++ src/page/product/index/index.jsx
@@ -1,77 +1,136 @@
 import React,{ Component } from 'react';
 import { Link } from "react-router-dom";
+import PageTitle from 'component/_pageTitle/index';
 
 import Pagination from 'utils/pagination/index';
 import TableList from 'utils/tableList/index';
-import PageTitle from 'component/_pageTitle/index';
 
-import MUtil from 'utils/index';
+import ListSearch from './index-list-search';
+import './index.scss';
+
+import MUtil from 'utils/mutil';
 import Product from 'service/productService';
 
-const _request = new MUtil();
+const _mutil = new MUtil();
 const _product = new Product();
 
 class ProductList extends Component {
   constructor(props){
-  super(props);
-  this.state = {
-    list: [],
-    pageNum:1,
-    firstLoading:true
+    super(props);
+    this.state = {
+      list: [],
+      pageNum:1,
+      listType: 'list'
+    }
   }
-}
 
   componentDidMount(){
     this._loadProductList();
   }
   
   _loadProductList(){
-    _product.getProductList(this.state.pageNum).then(res =>{
-      console.log(res)
-      this.setState(res,() => {
-        this.setState({
-          firstLoading:false
-        })
-      });
+    let listParam ={};
+    listParam.listType = this.state.listType;
+    listParam.pageNum = this.state.pageNum;
+
+    //如果是搜索的话，需要传入搜索类型和搜索关键字
+    if(this.state.listType === 'search'){
+      listParam.searchType = this.state.searchType;
+      listParam.keyword = this.state.searchKeyword;
+    }
+    
+    //请求接口
+    _product.getProductList(listParam).then(res =>{
+      this.setState(res);
     }, (errMsg) => {
       this.setState({
         list: []
       });
-      _request.errorTips(errMsg);
+      _mutil.errorTips(errMsg);
     })
   }
+  
+//搜索
+onSearch(searchType,searchKeyword){
+  // console.log(searchType,searchKeyword)
+  let listType = searchKeyword === '' ? 'list' : 'search';
+  this.setState({
+    listType       : listType,
+    pageNum        : 1,
+    searchType     : searchType,
+    searchKeyword  : searchKeyword
+  },() => {
+    this._loadProductList();
+  }) 
+}
 
 // 页数发生变化时，传入当前选中页面，调用this._loadProductList()刷新列表
   onPageNumChange(pageNum){
     this.setState({
       pageNum
-    },() => {
-      this._loadProductList()
+    }, () => {
+      this._loadProductList();
     })
   }
 
+// 改变商品状态:上架 / 下架
+  onSetProductStatus(e, productId, currentStatus){
+    let newStatus = currentStatus == 1 ? 2 : 1,
+        confirmTips = currentStatus == 1 
+        ? '确定要下架该商品？' 
+        : '确定要上架该商品？';
+    if(window.confirm(confirmTips)){
+      _product.setProductStatus({
+        productId: productId,
+        status: newStatus
+      }).then( res => {
+        _mutil.successTips(res);
+        this._loadProductList();
+      }, errMsg => {
+        _mutil.errorTips(errMsg);
+      })
+    }
+  }
+
   render(){
-    let listBody = this.state.list.map((product,index) => {
-      return (
-        <tr key={index}>
-          <td>{product.name}</td>
-          <td>{product.subtitle}</td>
-          <td>{product.id}</td>
-          <td>{product.categoryId}</td>
-          <td><img src={product.imageHost + product.mainImage} /></td>
-          <td>{product.price}</td>
-        </tr>
-      )
-    });
-
-
+    let tableHeads = [
+      {name:'商品ID',width:'10%'},
+      {name:'商品信息',width:'50%'},
+      {name:'价格',width:'10%'},
+      {name:'状态',width:'15%'},
+      {name:'操作',width:'15%'},
+      ]
     return(
-        <div id="page-wrapper">
+      <div id="page-wrapper">
         <div id="page-inner">
         <PageTitle title='商品列表'></PageTitle>
-        <TableList>
-
-        </TableList/>  
+        <ListSearch onSearch={(searchType,searchKeyword) => {this.onSearch(searchType,searchKeyword)}}/>
+        <TableList tableHeads={tableHeads}>
+          {
+            this.state.list.map((product,index) => {
+              return (
+                <tr key={index}>
+                  <td>{product.id}</td>
+                  <td>
+                    <p>{product.name}</p>
+                    <p>{product.subtitle}</p>
+                  </td>
+                  <td>￥{product.price}</td>
+                  <td>
+                    <p>{product.status == 1 ? '在售' : '已下架'}</p>
+                    <button 
+                      className='btn btn-warning btn-xs' 
+                      onClick={(e) => {this.onSetProductStatus(e,product.id,product.status)}}>{product.status == 1 ? '下架' : '上架'}</button>
+                  </td>
+                  <td>
+                    <Link className='oper' to={`/product/detail/${product.id}`}>详情</Link>
+                    <Link className='oper' to={`/product/save/${product.id}`}>编辑</Link>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </TableList>  
         <Pagination 
             current={this.state.pageNum} 
             total={this.state.total} 
--- src/service/productService.jsx
+++ src/service/productService.jsx
@@ -1,15 +1,36 @@
-import MUtil from 'utils/index';
-const _request = new MUtil();
+import MUtil from 'utils/mutil';
+const _mutil = new MUtil();
 
-class Product{
+// service封装,可以做多种请求
+class Product {
   // 获取商品列表
-  getProductList(pageNum){
-    return _request.request({
+  getProductList(listParam){
+    let url  = '',
+        data = {};
+
+    if(listParam.listType === 'list'){
+      url = '/manage/product/list.do';
+      data.pageNum = listParam.pageNum;
+    }
+    else if(listParam.listType === 'search'){
+      url = '/manage/product/search.do';
+      data.pageNum = listParam.pageNum;
+      data[listParam.searchType] = listParam.keyword;
+    }
+
+    return _mutil.request({
+      type : 'post',
+      url  : url,
+      data : data
+    })
+  }
+
+  // 变更商品销售状态
+  setProductStatus(productInfo){
+    return _mutil.request({
       type: 'post',
-      url: '/manage/product/list.do',
-      data: {
-        pageNum: pageNum
-      }
+      url: '/manage/product/set_sale_status.do',
+      data: productInfo
     })
   }
 }
