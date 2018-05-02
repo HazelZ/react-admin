e8e28778e391414bcf79ef3e92c4d8e8a30f4aab
HazelZ <525618432@qq.com>
2018-05-02 19:07:12 +0800

    category

--- src/page/product/router.jsx
+++ src/page/product/router.jsx
@@ -12,6 +12,7 @@ import ProductList from 'page/product/index/index';
 import ProductSave from 'page/product/index/save';
 import ProductDetail from 'page/product/index/detail';
 import CategoryList from 'page/product/category/index';
+import CategoryAdd from 'page/product/category/add';
 
 class ProductRouter extends Component {
   render(){
@@ -21,6 +22,8 @@ class ProductRouter extends Component {
         <Route path="/product/save/:pid?" component={ProductSave} />
         <Route path="/product/detail/:pid" component={ProductDetail} />
         <Route path="/product-category/index/:categoryId?" component={CategoryList} />
+        <Route path="/product-category/add" component={CategoryAdd} />
+
         <Redirect exact from="/product" to='/product/index' />
         <Redirect exact from="/product-category" to='/product-category/index' />
       </Switch>
